7e1bc6c8bb30a5cc37e6a0c9fece6adbb46ab6d2
Andre Scherma Soleo <andreschermas@gmail.com>
2016-01-13 14:27:48 -0200

    DatabaseManager Class writen

---
+++ LinguaDeGato/miniChallenge2/miniChallenge2/DatabaseManager.swift
@@ -0,0 +1,96 @@
+//
+//  DatabaseManager.swift
+//  LinguaDeGato
+//
+//  Created by Andre Scherma Soleo on 13/01/16.
+//  Copyright © 2016 Kobayashi. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DatabaseManager {
+    
+    // Database manager singleton
+    static let sharedInstance = DatabaseManager()
+    
+    
+    // The directory the application uses to store the Core Data store file.
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+    lazy var managedObjectModel: NSManagedObjectModel = {
+       let modelURL = NSBundle.mainBundle().URLForResource("LGdataModel", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    
+    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("LGdataModel.sqlite")
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        
+        do {
+            try coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        }
+        catch var error1 as NSError {
+            error = error1
+            coordinator = nil
+            
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            print("Unresolved error \(error), \(error!.userInfo)")
+        }
+        catch {
+            fatalError()
+        }
+        
+        return coordinator
+    }()
+    
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        
+        return managedObjectContext
+    }()
+    
+    func saveContext() {
+        if let moc = self.managedObjectContext {
+            var error: NSError? = nil
+            if moc.hasChanges {
+                do {
+                    try moc.save()
+                }
+                catch let error1 as NSError {
+                    error = error1
+                    // Replace this implementation with code to handle the error appropriately.
+                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    print("Unresolved error \(error), \(error!.userInfo)")
+                }
+            }
+        }
+    }
+}
